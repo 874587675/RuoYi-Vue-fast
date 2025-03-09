5e9596e9474af81c4ae5bdff8a4bbbb8827e54c7
874587675 <874587675@qq.com>
2025-03-09 12:30:07 +0800

    ry 修改内容

--- src/main/java/com/ruoyi/project/business/mapper/OrderSuccessMapper.java
+++
@@ -1,9 +0,0 @@
-package com.ruoyi.project.business.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.ruoyi.project.business.domain.OrderSuccess;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface OrderSuccessMapper extends BaseMapper<OrderSuccess> {
-}
