fc746ccae668b0f658fdb6dc1f8abcf676f0166b
874587675 <874587675@qq.com>
2025-03-23 12:35:39 +0800

    Revert "注释es"
    This reverts commit b5d96ac0800900922d57b50be2507ed8b61c846e.

--- src/main/java/com/ruoyi/project/business/service/ProductService.java
+++ src/main/java/com/ruoyi/project/business/service/ProductService.java
@@ -1,26 +1,26 @@
-//package com.ruoyi.project.business.service;
-//
-//import com.baomidou.mybatisplus.extension.service.IService;
-//import com.ruoyi.project.business.domain.Product;
-//import com.ruoyi.project.business.es.domain.ProductElasticsearch;
-//import org.springframework.data.elasticsearch.core.SearchHits;
-//
-//import java.util.List;
-//
-///**
-//* @program: RuoYi-Vue-fast
-//* @ClassName EsProductService
-//* @description:
-//* @author: zgc
-//* @date: 2025-03-06 18:16
-//* @Version 1.0
-//**/
-//public interface ProductService extends IService<Product>{
-//    void syncProductsToElasticsearch();
-//
-//    List<ProductElasticsearch> searchProducts(String query);
-//
-//    SearchHits<ProductElasticsearch> searchLikeProducts(String queryString);
-//
-//    SearchHits<ProductElasticsearch> searchFuzzyQuery(String queryString);
-//}
+package com.ruoyi.project.business.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.ruoyi.project.business.domain.Product;
+import com.ruoyi.project.business.es.domain.ProductElasticsearch;
+import org.springframework.data.elasticsearch.core.SearchHits;
+
+import java.util.List;
+
+/**
+* @program: RuoYi-Vue-fast
+* @ClassName EsProductService
+* @description: 
+* @author: zgc
+* @date: 2025-03-06 18:16
+* @Version 1.0
+**/
+public interface ProductService extends IService<Product>{
+    void syncProductsToElasticsearch();
+
+    List<ProductElasticsearch> searchProducts(String query);
+
+    SearchHits<ProductElasticsearch> searchLikeProducts(String queryString);
+
+    SearchHits<ProductElasticsearch> searchFuzzyQuery(String queryString);
+}
